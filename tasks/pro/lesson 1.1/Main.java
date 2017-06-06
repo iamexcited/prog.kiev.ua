5f13bfbe95953505e661af1d1585e6bc83b5dc83
iamexcited <sqatesterone@gmail.com>
2017-06-06 13:42:25 +0300

    Create Main.java

---
+++ tasks/pro/lesson 1.1/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    
+    {
+        try
+        
+        {
+            
+            Tester.test(MyClass.class);
+            
+        }
+        
+        catch(Exception e)
+        
+        {
+        
+            e.printStackTrace();
+            
+        }
+    }
+}
