5f5228b40b74b0bb8845e330857a9e5dc7713a28
iamexcited <sqatesterone@gmail.com>
2017-06-06 13:49:31 +0300

    Update Test.java

--- tasks/pro/lesson 1.1/Test.java
+++ tasks/pro/lesson 1.1/Test.java
@@ -1,13 +1,11 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+package tasks.pro.lesson_1_1
+
+import java.lang.annotation.*;
 
 
-@Target(value= ElementType.METHOD)
 @Retention(value= RetentionPolicy.RUNTIME)
 @interface Test  {
-    int a() default 1;
+    int a();
     int b();
 
 }
